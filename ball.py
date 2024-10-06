766b0cc031e57b817d5b0bbbaf52251d487647a8
Kunal <kunaldubey90178@gmail.com>
2024-10-06 09:40:13 +0530

    final

--- ball.py
+++ ball.py
@@ -21,9 +21,10 @@ class Ball(Turtle):
         
     def bounce_x(self):
         self.x_move *= -1
-        self.move_speed-=0.05
+        self.move_speed*=0.9
 
     def reset_position(self):
         self.move_speed = 0.2
         self.goto(0, 0)  # Reset ball to the center
         self.bounce_x()  # Change ball direction after reset
+
